11cd73211ecc937101979fc4e6a7b98be083ca60
maddsua <maddsuadev@pm.me>
2026-05-02 19:24:47 +0200

    add a/b card switching and animations

--- src/components/Cards/CardView.vue
+++ src/components/Cards/CardView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { nextTick, reactive, ref } from 'vue';
 import type { CardNode } from './content';
 import Card from './Card.vue';
 
@@ -12,11 +12,121 @@ const emit = defineEmits<{
 }>();
 
 const activeIdx = ref(0);
-const activeCard = computed(() => props.entries[activeIdx.value]);
+
+interface CardState {
+	card: CardNode;
+	flags: {
+		active: boolean;
+		animate?: boolean;
+		fade?: boolean;
+		swipe_up?: boolean;
+		swipe_down?: boolean;
+	}
+};
+
+type SlideInDirection = 'from-top' | 'from-bottom';
+type SlideOutDirection = 'to-top' | 'to-bottom';
+
+const slideOut = (direction?: SlideInDirection): SlideOutDirection | undefined => direction === 'from-bottom' ? 'to-top' : direction === 'from-top' ? 'to-bottom' : undefined;
+
+const newAnimatedState = (direction?: SlideInDirection): CardState => {
+
+	const nextCard = props.entries[activeIdx.value];
+	const state: CardState = {
+		card: nextCard,
+		flags: {
+			active: true,
+		}
+	};
+
+	switch (direction) {
+		case 'from-top':
+
+			state.flags.swipe_up = true;
+			state.flags.fade = true;
+
+			nextTick(() => {
+				state.flags.animate = true;
+				nextTick(() => {
+					state.flags.swipe_up = false;
+					state.flags.fade = false;
+				});
+			});
+
+			break;
+	
+		case 'from-bottom':
+
+			state.flags.swipe_down = true;
+			state.flags.fade = true;
+
+			nextTick(() => {
+				state.flags.animate = true;
+				nextTick(() => {
+					state.flags.swipe_down = false;
+					state.flags.fade = false;
+				});
+			});
+
+			break;
+	}
+
+	return state;
+}
+
+const pairState = reactive({
+	a: newAnimatedState(),
+	b: null as CardState | null,
+});
+
+const ejectAnimatedState = async (state: CardState, direction?: SlideOutDirection) => {
+
+	state.flags.animate = true;
+	
+	nextTick(() => {
+
+		state.flags.active = false;
+
+		if (!direction) {
+			state.flags.fade = true;
+			return;
+		}
+
+		switch (direction) {
+			case 'to-bottom':
+				state.flags.swipe_down = true;
+				break;
+		
+			case 'to-top':
+				state.flags.swipe_up = true;
+				break;
+		}
+
+		setTimeout(() => state.flags.fade = true, 200);
+	});
+};
+
+const switchCards = (direction?: SlideInDirection) => {
+
+	if (!pairState.b) {
+		ejectAnimatedState(pairState.a, slideOut(direction));
+		pairState.b = newAnimatedState(direction);
+		return
+	}
+
+	if (pairState.a.flags.active) {
+		ejectAnimatedState(pairState.a, slideOut(direction));
+		pairState.b = newAnimatedState(direction);
+	} else {
+		ejectAnimatedState(pairState.b, slideOut(direction));
+		pairState.a = newAnimatedState(direction);
+	}
+};
 
 const nextCard = () => {
 	if (activeIdx.value < props.entries.length - 1) {
 		activeIdx.value++
+		switchCards('from-bottom');
 		return;
 	}
 
@@ -26,27 +136,74 @@ const nextCard = () => {
 const prevCard = () => {
 	if (activeIdx.value > 0) {
 		activeIdx.value--
+		switchCards('from-top');
 	}
 };
-
-//	todo: animate A/B
 
 </script>
 
 <template>
 	<div class="card-view">
-		<Card :key="activeCard.id" :card="activeCard" @next="nextCard" @prev="prevCard" />
+		<template v-for="(item,idx) of [pairState.a, pairState.b]" :key="`${idx}:${item?.card.id}`">
+			<div class="card-slot" :class="item?.flags">
+				<Card v-if="item" :key="item.card.id" :card="item.card" @next="nextCard" @prev="prevCard" />
+			</div>
+		</template>
+		<div class="controls">
+			<button type="button" @click="prevCard">Prev</button>
+			<button type="button" @click="nextCard">Next</button>
+		</div>
 	</div>
 </template>
 
 <style lang="scss" scoped>
 	.card-view {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
 		position: relative;
 		width: 100%;
 		height: 100%;
 		overflow: hidden;
+
+		.card-slot {
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			overflow: hidden;
+			
+			&.animate {
+				transition: all 200ms ease;
+			}
+
+			&.active {
+				z-index: 1;
+			}
+
+			&.swipe_up {
+				transform: translateX(25vw) translateY(-100vh);
+			}
+
+			&.swipe_down {
+				transform: translateX(-25vw) translateY(100vh);
+			}
+
+			&.fade {
+				opacity: 0;
+			}
+		}
+
+		//	todo: rm
+		.controls {
+			position: absolute;
+			bottom: 0;
+			left: 0;
+			width: 100%;
+			display: flex;
+			justify-content: space-around;
+			z-index: 20;
+		}
 	}
 </style>
