4d03740a76a22e5e5388c54b14ef94c130ddc743
maddsua <maddsuadev@pm.me>
2026-05-02 16:40:04 +0200

    handle swap gestures

--- src/components/Cards/Card.vue
+++ src/components/Cards/Card.vue
@@ -12,17 +12,70 @@ const emit = defineEmits<{
 	(e: 'next'): void;
 }>();
 
-//	todo: handle swiping
-
 const flipped = ref(false);
 const randomRotate = computed(() => (Math.random() - 0.5) * 6);
 
 const flip = () => flipped.value = !flipped.value;
 
+const dragState = ref<{
+	initX: number;
+	initY: number;
+	x: number;
+	y: number;
+} | null>(null);
+
+const transformStyle = computed(() => ({
+	rotate: dragState.value ? `${((dragState.value.x - dragState.value.initX) / 100).toFixed(1)}deg` : `${randomRotate.value.toFixed(1)}deg`,
+	transform: dragState.value ? `translateX(${dragState.value.x - dragState.value.initX}px) translateY(${dragState.value.y - dragState.value.initY}px)` : undefined,
+}));
+
+const handleDragStart = (event: PointerEvent) => {
+
+	const { clientX, clientY } = event;
+
+	dragState.value = {
+		initX: clientX,
+		initY: clientY,
+		x: clientX,
+		y: clientY,
+	};
+};
+
+const handleDragUpdate = (event: PointerEvent) => {
+
+	if (!dragState.value) {
+		return;
+	}
+
+	const { clientX, clientY } = event;
+
+	dragState.value.x = clientX;
+	dragState.value.y = clientY;
+};
+
+const handleDragDone = () => {
+
+	if (!dragState.value) {
+		return;
+	}
+
+	const delta = dragState.value.initY - dragState.value.y;
+
+	if (Math.abs(delta) > 50) {
+		if (delta > 0) {
+			emit('next');
+		} else {
+			emit('prev');
+		}
+	}
+
+	dragState.value = null;
+};
+
 </script>
 
 <template>
-	<div class="card-container" :class="{ flipped }" :style="{ rotate: `${randomRotate.toFixed(1)}deg` }" @flip="flip">
+	<div class="card-container" :class="{ flipped, dragging: !!dragState }" :style="transformStyle" @pointerdown="handleDragStart" @pointermove="handleDragUpdate" @pointerup="handleDragDone" @pointercancel="handleDragDone" @pointerout="handleDragDone" @flip="flip">
 		<CardFace :entry="card.front" @flip="flip" @next="emit('next')" />
 		<CardFace :entry="card.back" @flip="flip" @next="emit('next')" />
 	</div>
@@ -45,6 +98,10 @@ const flip = () => flipped.value = !flipped.value;
 
 		&.flipped {
 			transform: rotateY(180deg);
+		}
+
+		&.dragging {
+			transition: none;
 		}
 	}
 	
--- src/components/Cards/CardView.vue
+++ src/components/Cards/CardView.vue
@@ -41,5 +41,6 @@ const prevCard = () => {
 		position: relative;
 		width: 100%;
 		height: 100%;
+		overflow: hidden;
 	}
 </style>
