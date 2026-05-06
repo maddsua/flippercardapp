ee4be1aa8d4576f371e6f7e98966e0efdae766ad
maddsua <maddsuadev@pm.me>
2026-05-06 16:13:50 +0200

    redirect to the home view instead of a 404 page

--- src/components/Home/HomeView.vue
+++ src/components/Home/HomeView.vue
@@ -1,23 +1,39 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
+import GenericButton from '../App/GenericButton.vue';
+
+const router = useRouter();
+
+const openApp = () => {
+	router.push('/app');
+};
 
 </script>
 
 <template>
 
 	<div class="home-view">
-		<p>
+		<h1>
 			Home view
+		</h1>
+
+		<p>
+			TODO: add a small presentation here
 		</p>
 
 		<p>
-			<RouterLink to="/app/collections">
-				Show collections
-			</RouterLink>
+			<GenericButton @click="openApp">
+				Open App
+			</GenericButton>
 		</p>
 	</div>
 
 </template>
 
 <style lang="scss" scoped>
-
+	.home-view {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,17 @@ import PlayView from './components/Play/PlayView.vue';
 import HomeView from './components/Home/HomeView.vue';
 import CollectionsListView from './components/Collections/CollectionsListView.vue';
 import CollectionView from './components/Collections/CollectionView.vue';
-import NotFoundView from './components/NotFound/NotFoundView.vue';
 
 import './main.scss';
 import './theme.scss';
 
 const routes = [
 	{ path: '/', component: HomeView },
+	{ path: '/app', component: CollectionsListView },
 	{ path: '/app/collections', component: CollectionsListView },
 	{ path: '/app/collection/:collection_id', component: CollectionView },
 	{ path: '/app/play/deck/:deck_id', component: PlayView },
-	{ path: '/:pathMatch(.*)*', component: NotFoundView },
+	{ path: '/:pathMatch(.*)*', component: HomeView },
 ]
 
 const router = createRouter({
